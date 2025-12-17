3f113345b53602f12000a979c2fa5fc09f0b7975
Christian Clavite <156822259+XTian-Clav@users.noreply.github.com>
2025-12-17 17:02:23 +0800

    minor fix to border style for guidelines

--- resources/views/filament/portal/pages/guidelines.blade.php
+++ resources/views/filament/portal/pages/guidelines.blade.php
@@ -1,11 +1,12 @@
 <x-filament-panels::page>
-    <div style="
+<div style="
     font-family: 'Poppins';
-    border: 1.5pt solid #aaa;
-    padding: 15pt; 
-    background-color: white;
-    border-radius: 16px;
-    margin: 10pt 0;
+    background-color: #ffffff;
+    border: 1px solid #e5e7eb;
+    border-radius: 12px;
+    padding: 20pt;
+    margin: 15pt 0;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px -1px rgba(0, 0, 0, 0.1);
 ">
     @include('pdf-template.pdf-template-guidelines')
 </div>
